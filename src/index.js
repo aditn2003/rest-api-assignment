df4559cfca43e1d95ba882d17158af8e17b70d38
Adit Nuwal <an238@njit.edu>
2025-09-30 16:09:18 -0400

    API working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const port = 3000;
 
@@ -8,19 +9,79 @@ app.use(express.json());
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
+const users = [];
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({ error: "Name and Email required" });
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    email,
+  };
+  users.push(newUser);
+  return res.status(201).json(newUser);
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((u) => u.id === id);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  return res.json(user);
+});
+
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+
+  const user = users.find((u) => u.id === id);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  if (!name || !email) {
+    return res.status(400).json({ error: "Name and email are required" });
+  }
+
+  user.name = name;
+  user.email = email;
+
+  return res.json(user);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const index = users.findIndex((u) => u.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  users.splice(index, 1);
+
+  return res.status(204).send();
 });
 
 // Do not touch the code below this comment
 // **************************************************************
 
 // Start the server (only if not in test mode)
-if (process.env.NODE_ENV !== 'test') {
-    app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
 }
 
 module.exports = app; // Export the app for testing
